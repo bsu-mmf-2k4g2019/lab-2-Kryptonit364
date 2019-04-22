136c31202bc0df92f2510b4662d9a07138671c83
Max <47553356+Kryptonit364@users.noreply.github.com>
2019-04-23 00:42:33 +0300

    v1.2.1
    - changed background color

--- glwidget.cpp
+++ glwidget.cpp
@@ -506,7 +506,7 @@ void GLWidget::paintGL()
 {
     if (autoRotate) noTime(t_x, t_y, t_z);
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.95f, 0.95f, 0.95f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glActiveTexture(GL_TEXTURE0);
